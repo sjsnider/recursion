3875fde9c4dea5326e8314675ffe777c834cb56d
Scott Snider <sjsnider@gmail.com>
2014-03-28 01:51:04 -0700

    got recursion working with getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,22 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  var array = [];
+  
+  	function recurse(node){
+  		var nodes = node.childNodes;
+  		
+  		for (var i=0; i<nodes.length; i++ ){
+  			if (typeof nodes[i].classList !== "undefined") {
+  				if (nodes[i].classList.contains(className)){
+					array.push(nodes[i]);
+				}
+				recurse(nodes[i]);
+			}
+		}
+		
+  	}
+
+  	recurse(document.body);
+  	return array;
 };
