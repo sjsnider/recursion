3e0509319c5fa37885d6f636f9753508f42232f5
Scott Snider <sjsnider@gmail.com>
2014-03-30 12:24:36 -0700

    handles object with no properties, added some comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,29 +5,31 @@
 // but you don't so you're going to have to write it from scratch:
 
 var stringifyJSON = function (obj) {
-  // your code goes here
   var recurseString = "";
- // console.log(obj);
 
   function recurse (recurseObj) {
   //	console.log(typeof recurseObj);
   	var string = "";
+  	//handles numbers and booleans
   	if (typeof recurseObj === 'number' || typeof recurseObj === 'boolean')
   		string += recurseObj;
+  	// handles null
   	else if (recurseObj === null )
   		string += null;
+  	// handles strings
   	else if (typeof recurseObj === 'string')
   		string += '"' + recurseObj + '"';
+  	// handles objects
   	else if (typeof recurseObj === 'object'){
   		if(recurseObj.length === 0)
   			string += '[]';
+  		// check if the object contains any properties
+  		else if (isEmpty(recurseObj))
+  			string += '{}';
   		else {
   			var loopString = "";
   			for (var i in recurseObj){
   				loopString += recurse(recurseObj[i]) + ',';
-  			//	delete recurseObj[i];
-  				//console.log(recurseObj);
-  				//recurse(recurseObj);
   			}
   			loopString = loopString.slice(0,-1);
 
@@ -41,3 +43,12 @@ var stringifyJSON = function (obj) {
   return finalString;
 };
 
+// checks if an object has an properties
+function isEmpty(map) {
+   for(var key in map) {
+      if (map.hasOwnProperty(key)) {
+         return false;
+      }
+   }
+   return true;
+}
